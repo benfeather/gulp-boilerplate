59fc3dd167c08e432843a63ab3c78685a5a41f12
Ben Feather <43571410+benfeather@users.noreply.github.com>
2019-12-22 16:18:21 +1300

    Formatting

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,42 @@
 // --------------------------------------------------
 
 // General
-const gulp          = require('gulp');
-const rename        = require('gulp-rename');
-const sourcemaps    = require('gulp-sourcemaps');
-const plumber       = require('gulp-plumber');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
 
-const { format }    = require('string-kit') ;
-const { pipeline }  = require('readable-stream');
+const { format } = require('string-kit');
+const { pipeline } = require('readable-stream');
 
 // CSS
-const sass          = require('gulp-sass');
-const postcss       = require('gulp-postcss');
-const autoprefixer  = require('autoprefixer');
-const cssnano       = require('cssnano');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
 // JS
-const concat        = require('gulp-concat');
-const babel         = require('gulp-babel');
-const terser        = require('gulp-terser');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
 
 // --------------------------------------------------
 // Settings & Configuration
 // --------------------------------------------------
 
 const styles = {
-    dest: 'dist/css/',
-    bundles: {
-        'styles': 'src/sass/**/*.{scss,sass}'
-    }
+	dest: 'dist/css/',
+	bundles: {
+		styles: 'src/sass/**/*.{scss,sass}'
+	}
 };
 
 const scripts = {
-    dest: 'dist/js/',
-    bundles: {
-        'main': [
-            'src/js/script-1.js',
-            'src/js/script-2.js',
-        ],
-        'vendor': [
-            'src/js/script-3.js'
-        ]
-    }
+	dest: 'dist/js/',
+	bundles: {
+		main: ['src/js/script-1.js', 'src/js/script-2.js'],
+		vendor: ['src/js/script-3.js']
+	}
 };
 
 const buildTasks = [];
@@ -57,15 +52,16 @@ const watchTasks = [];
 // --------------------------------------------------
 
 const error = function(err) {
-    const message = '\n' +
-                    `^rError:^ ${err.plugin} ran into an error` +
-                    '\n' +
-                    `${err.messageOriginal}` +
-                    '\n' +
-                    `^rline ${err.line}^ in ^r${err.relativePath}^` +
-                    '\n';
+	const message =
+		'\n' +
+		`^rError:^ ${err.plugin} ran into an error` +
+		'\n' +
+		`${err.messageOriginal}` +
+		'\n' +
+		`^rline ${err.line}^ in ^r${err.relativePath}^` +
+		'\n';
 
-    console.log(format(message));
+	console.log(format(message));
 };
 
 // --------------------------------------------------
@@ -73,128 +69,125 @@ const error = function(err) {
 // --------------------------------------------------
 
 const cssBuildTask = function(task) {
-    gulp.task(task.buildName, function() {
-        return pipeline(
-            // Get the source files
-            gulp.src(task.files),
-            
-            // Init the custom error handling
-            plumber({ 
-                'errorHandler': error 
-            }),
+	gulp.task(task.buildName, function() {
+		return pipeline(
+			// Get the source files
+			gulp.src(task.files),
 
-            // Init the sourcemap
-            sourcemaps.init(),
-            
-            // Compile sass
-            sass(),
-            
-            // Apply the PostCSS processors
-            postcss([
-                autoprefixer,
-                cssnano
-            ]),
-            
-            // Rename the file with the 'min' suffix
-            rename({
-                basename: task.name, 
-                suffix: '.min' 
-            }),
-            
-            // Output the sourcemap
-            sourcemaps.write('.'),
-            
-            // Output the compiled css
-            gulp.dest(styles.dest)
-        );
-    });
+			// Init the custom error handling
+			plumber({
+				errorHandler: error
+			}),
+
+			// Init the sourcemap
+			sourcemaps.init(),
+
+			// Compile sass
+			sass(),
+
+			// Apply the PostCSS processors
+			postcss([autoprefixer, cssnano]),
+
+			// Rename the file with the 'min' suffix
+			rename({
+				basename: task.name,
+				suffix: '.min'
+			}),
+
+			// Output the sourcemap
+			sourcemaps.write('.'),
+
+			// Output the compiled css
+			gulp.dest(styles.dest)
+		);
+	});
 };
 
 const cssWatchTask = function(task) {
-    gulp.task(task.watchName, function() {
-        return gulp.watch(task.files, gulp.series(task.buildName));
-    });
+	gulp.task(task.watchName, function() {
+		return gulp.watch(task.files, gulp.series(task.buildName));
+	});
 };
 
 for (const [name, files] of Object.entries(styles.bundles)) {
-    const task = {
-        'buildName': 'build:css:' + name,
-        'watchName': 'watch:css:' + name,
-        'name': name,
-        'files': files
-    };
+	const task = {
+		buildName: 'build:css:' + name,
+		watchName: 'watch:css:' + name,
+		name: name,
+		files: files
+	};
 
-    cssBuildTask(task);
-    cssWatchTask(task);
+	cssBuildTask(task);
+	cssWatchTask(task);
 
-    buildTasks.push(task.buildName);
-    watchTasks.push(task.watchName);
-};
+	buildTasks.push(task.buildName);
+	watchTasks.push(task.watchName);
+}
 
 // --------------------------------------------------
 // Gulp: Compile JavaScript
 // --------------------------------------------------
 
 const jsBuildTask = function(task) {
-    gulp.task(task.buildName, function() {
-        return pipeline(
-            // Get the source files
-            gulp.src(task.files),
-            
-            // Init the custom error handling
-            plumber({ 
-                'errorHandler': error 
-            }),
+	gulp.task(task.buildName, function() {
+		return pipeline(
+			// Get the source files
+			gulp.src(task.files),
 
-            // Init the sourcemap
-            sourcemaps.init(),
-            
-            // Combine the files and rename
-            concat(task.name + '.min.js'),
+			// Init the custom error handling
+			plumber({
+				errorHandler: error
+			}),
 
-            // Babel
-            babel({
-                presets: ['@babel/env']
-            }),
-            
-            // Minify
-            terser(),
+			// Init the sourcemap
+			sourcemaps.init(),
 
-            // Write the sourcemap
-            sourcemaps.write('.'),
-            
-            // Output the compiled js
-            gulp.dest(scripts.dest)
-        );
-    });
+			// Combine the files and rename
+			concat(task.name + '.min.js'),
+
+			// Babel
+			babel({
+				presets: ['@babel/env']
+			}),
+
+			// Minify
+			terser(),
+
+			// Write the sourcemap
+			sourcemaps.write('.'),
+
+			// Output the compiled js
+			gulp.dest(scripts.dest)
+		);
+	});
 };
 
 const jsWatchTask = function(task) {
-    gulp.task(task.watchName, function() {
-        return gulp.watch(task.files, gulp.series(task.buildName));
-    });
+	gulp.task(task.watchName, function() {
+		return gulp.watch(task.files, gulp.series(task.buildName));
+	});
 };
 
 for (const [name, files] of Object.entries(scripts.bundles)) {
-    const task = {
-        'buildName': 'build:js:' + name,
-        'watchName': 'watch:js:' + name,
-        'name': name,
-        'files': files
-    };
+	const task = {
+		buildName: 'build:js:' + name,
+		watchName: 'watch:js:' + name,
+		name: name,
+		files: files
+	};
 
-    jsBuildTask(task);
-    jsWatchTask(task);
+	jsBuildTask(task);
+	jsWatchTask(task);
 
-    buildTasks.push(task.buildName);
-    watchTasks.push(task.watchName);
-};
+	buildTasks.push(task.buildName);
+	watchTasks.push(task.watchName);
+}
 
 // --------------------------------------------------
 // Gulp: Default
 // --------------------------------------------------
 
 exports.default = gulp.series(
-    gulp.parallel(buildTasks),
-    gulp.parallel(watchTasks),
+	gulp.parallel(buildTasks),
+	gulp.parallel(watchTasks)
 );
